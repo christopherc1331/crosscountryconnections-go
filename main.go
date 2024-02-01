ed296afa98f935a9cc295c0ed6216aaba043b225
Chris <christopherc1331@gmail.com>
2024-01-31 23:11:28 -0600

    Added template rendering to server

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
 	fmt.Println("Hello World!")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello World")
-		io.WriteString(w, r.Method)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			}}
+		tmpl.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
 
